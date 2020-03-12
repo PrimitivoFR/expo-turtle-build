8b12fa07a1005b5613ada614201cdd696630d021
AppliNH <thomas.martindev@gmail.com>
2020-03-12 16:15:49 +0100

    Tag 0.5 on DockerHub+  fully working in production for us

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,12 @@ RUN apt-get -y update
 RUN apt-get -y install build-essential
 RUN apt-get -y install openjdk-8-jdk openjdk-8-jre
 # RUN apt-get -y install npm && apt -y install git 
-RUN apt-get -y install curl && curl -sL https://deb.nodesource.com/setup_12.x | bash
-RUN apt-get -y install nodejs && apt-get -y install git 
+RUN apt-get -y install curl && curl -sL https://deb.nodesource.com/setup_12.x | bash -
+RUN apt-get -y install nodejs && apt-get -y install git
 RUN npm i -g turtle-cli --unsafe-perm
 RUN chmod u+x /usr/src/app/shExpoBuild
 RUN chmod u+x /usr/src/app/shExpoBuild/hi.sh
 RUN chmod u+x /usr/src/app/shExpoBuild/buildAndroid.sh
 RUN npm i -g expo-cli
 
-ENTRYPOINT "/usr/src/app/shExpoBuild/hi.sh" && /bin/sh
+ENTRYPOINT "/usr/src/app/shExpoBuild/hi.sh" && /bin/bash
--- sources/buildAndroid.sh
+++ sources/buildAndroid.sh
@@ -5,19 +5,24 @@
 now=$(date +'%m_%d_%Y')
 timestamp=$(date +%s)
 
-export EXPO_ANDROID_KEYSTORE_PASSWORD=$1
-export EXPO_ANDROID_KEY_PASSWORD=$2
+EXPO_ANDROID_KEYSTORE_PASSWORD=$1
+EXPO_ANDROID_KEY_PASSWORD=$2
 
-expoUsername = $3
-expoPassword = $4
-pathToYourJKS = $5
-keystoreAlias = $6
+expoUsername=$3
+expoPassword=$4
+pathToYourJKS=$5
+keystoreAlias=$6
+
+export EXPO_ANDROID_KEYSTORE_PASSWORD=$EXPO_ANDROID_KEYSTORE_PASSWORD
+export EXPO_ANDROID_KEY_PASSWORD=$EXPO_ANDROID_KEY_PASSWORD
+
+
 # expo publish
 
 turtle setup:android
 
-turtle build:android --username expoUsername --password expoPassword \
---keystore-path pathToYourJKS --keystore-alias keystoreAlias  \
+turtle build:android --username $expoUsername --password $expoPassword \
+--keystore-path $pathToYourJKS --keystore-alias $keystoreAlias  \
 -o ~/expo-apps/android/$now/YOUR_APP_android_$timestamp.aab
 # If you don't use --public-url here^ I think it's better that you login to expo before
 # starting the build (with expo login) and uncomment the "expo publish" line
