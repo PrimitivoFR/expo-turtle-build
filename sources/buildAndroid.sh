3589e03daa4c9d94c54af39ae3a1bc7c8509a119
Primitivo <primitivo@macbook-pro-de-macbook.home>
2020-03-18 23:10:33 +0100

    added the possibility to build .aab OR .apk

--- sources/buildAndroid.sh
+++ sources/buildAndroid.sh
@@ -12,6 +12,13 @@ expoUsername=$3
 expoPassword=$4
 pathToYourJKS=$5
 keystoreAlias=$6
+target=$7
+
+if [ "$target" == "apk" ]; then
+    extension="apk"
+else
+    extension="aab"
+fi
 
 export EXPO_ANDROID_KEYSTORE_PASSWORD=$EXPO_ANDROID_KEYSTORE_PASSWORD
 export EXPO_ANDROID_KEY_PASSWORD=$EXPO_ANDROID_KEY_PASSWORD
@@ -21,8 +28,9 @@ export EXPO_ANDROID_KEY_PASSWORD=$EXPO_ANDROID_KEY_PASSWORD
 
 turtle setup:android
 
-turtle build:android --username $expoUsername --password $expoPassword \
+turtle build:android -t $target --username $expoUsername --password $expoPassword \
 --keystore-path $pathToYourJKS --keystore-alias $keystoreAlias  \
--o ~/expo-apps/android/$now/YOUR_APP_android_$timestamp.aab
+-o ./expo-apps/android/$now/YOUR_APP_android_$timestamp.$extension
+
 # If you don't use --public-url here^ I think it's better that you login to expo before
 # starting the build (with expo login) and uncomment the "expo publish" line
